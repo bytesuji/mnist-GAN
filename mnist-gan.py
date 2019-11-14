083eef53a6c4c2ea5c91e2ef22930553d5301dae
Albert Brox <albert@exypno.tech>
2019-11-14 17:58:10 -0500

    add MNISTGenerator, partial MNISTGAN

--- mnist-gan.py
+++ mnist-gan.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot
 
 class MNISTDiscriminator(object):
     ## Takes a 28x28 image, gives a binary classification (real or fake)
-    def create_discriminator(self, input_shape=(28, 28, 1)):
+
+    def __init__(self, input_shape=(28, 28, 1)):
         discrim_layers = [
             layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same',
                           input_shape=input_shape),
@@ -21,16 +22,10 @@ class MNISTDiscriminator(object):
             layers.Dense(1, activation='sigmoid')
         ]
 
-        discriminator = K.models.Sequential(discrim_layers)
-        discriminator.compile(loss='binary_crossentropy',
-                      optimizer=K.optimizers.Adam(lr=0.002, beta_1=0.5),
-                      metrics=['accuracy'])
-
-        return discriminator
-
-
-    def __init__(self):
-        self.model = self.create_discriminator()
+        self.model = K.models.Sequential(discrim_layers)
+        self.model.compile(loss='binary_crossentropy',
+                              optimizer=K.optimizers.Adam(lr=0.002, beta_1=0.5),
+                              metrics=['accuracy'])
 
 
     def create_real_samples(self, data, n):
@@ -47,9 +42,9 @@ class MNISTDiscriminator(object):
         return x, y
 
 
-    def train(self, data, iterations=128, batch_size=256):
+    def train(self, data, epochs=128, batch_size=256):
         print("--- TRAIN DISCRIMINATOR ---")
-        for i in range(iterations):
+        for i in range(epochs):
             x_real, y_real = self.create_real_samples(data, int(batch_size / 2))
             x_fake, y_fake = self.create_fake_samples(int(batch_size / 2))
 
@@ -57,7 +52,54 @@ class MNISTDiscriminator(object):
             fake_acc = self.model.train_on_batch(x_fake, y_fake)[1]
             avg_acc = (real_acc + fake_acc) / 2
 
-            print("iteration {} accuracy {}%".format(i, avg_acc*100))
+            print("epoch {} accuracy {}%".format(i, avg_acc*100))
+
+
+class MNISTGenerator(object):
+    def __init__(self, latent_space_dim=100, init_dim=(7, 7, 128)):
+        self.latent_space_dim = latent_space_dim
+        init_dim_m = init_dim[0] * init_dim[1] * init_dim[2]
+
+        gen_layers = [
+            layers.Dense(init_dim_m, input_dim=latent_space_dim),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Reshape(init_dim),
+            layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(1, (7, 7), activation='sigmoid', padding='same')
+        ]
+
+        self.model = K.models.Sequential(gen_layers)
+
+    def generate_latent_element(self, n):
+        return np.random.randn(self.latent_space_dim * n).reshape(n, latent_space_dim)
+
+    def generate(self, n):
+        latent = self.generate_latent_element(n)
+        x = self.model.predict(latent)
+        y = np.zeros((n, 1))
+
+        return x, y
+
+        
+class MNISTGAN(class):
+    def __init__(self):
+        self.discriminator = MNISTDiscriminator()
+        self.discriminator.model.trainable = False
+        self.generator = MNISTGenerator()
+
+        self.gan = K.models.Sequential()
+        self.gan.add(self.discriminator.model)
+        self.gan.add(self.generator.model)
+
+        self.gan.compile(loss='binary_crossentropy',
+                         optimizer=K.optimizers.Adam(lr=0.0002, beta_1=0.5))
+
+
+    def train(self, epochs=100, batch_size=256):
+        for i in range(epochs):
 
 
 
